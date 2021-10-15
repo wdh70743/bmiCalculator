8e88bf95eb8297a4d03f9793f76294c903c0add0
wdh70743 <wdh70743@gmail.com>
2021-10-15 11:44:08 +0900

    bmi calculator

---
+++ app/src/main/java/com/wdh/bmi_calculator/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.wdh.bmi_calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.wdh.bmi_calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        getResult()
+    }
+
+    private fun getResult(){
+        binding.resultButton.setOnClickListener {
+
+            if (binding.heightEditText.text.isBlank() || binding.weightEditText.text.isBlank()){
+                Toast.makeText(this, "값을 입력해주세요", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+
+            val height:Int = binding.heightEditText.text.toString().toInt()
+            val weight:Int = binding.heightEditText.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+            startActivity(intent)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/wdh/bmi_calculator/ResultActivity.kt
@@ -0,0 +1,40 @@
+package com.wdh.bmi_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wdh.bmi_calculator.databinding.ActivityResultBinding
+import kotlin.math.pow
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setResult()
+    }
+
+    private fun setResult(){
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        val bmi = weight / (height / 100.0).pow(2.0)
+
+        val resultText = when{
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 23.0 -> "과체중"
+            bmi >= 18.5 -> "정상체중"
+            else -> "저체중"
+        }
+
+        binding.bmiResultTextView.text = bmi.toString()
+        binding.resultTextView.text = resultText
+    }
+}
